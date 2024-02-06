7e8e0941ac9c959721071e7ab63fe7ef4f6bde59
JuanEladio04 <113886575+JuanEladio04@users.noreply.github.com>
2024-02-06 10:27:02 +0100

    feat(proyect) securityBackup

---
+++ app/Models/Car.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Car extends Model
+{
+    use HasFactory, softDeletes;
+
+    protected $fillable = [
+        'plate',
+        'marca',
+        'model',
+        'year',
+        'last_revision_date',
+        'photo',
+        'price',
+        'user_id'
+    ];
+
+    public function propietario(){
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
